3e9a4f0a7acf22c240f3e35ca0d93357a904667f
Your Name <you@example.com>
2020-08-04 19:36:02 +0300

    simple shooting system done

---
+++ Assets/Scripts/BulletMovement.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMovement : MonoBehaviour
+{
+    private void Update()
+    {
+        transform.Translate(Vector2.right * 15f * Time.deltaTime);
+    }
+
+}
---
+++ Assets/Scripts/PlayerWeapon.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWeapon : MonoBehaviour
+{
+    public GameObject projectile;
+    public Transform firePoint;
+    public Transform weaponPoint;
+
+    private float fireRate = 3f;
+    private float fireTime;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        RotateToMouse();
+        if (Input.GetMouseButton(0) && CanShoot())
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        Instantiate(projectile, firePoint.position, weaponPoint.rotation);
+        fireTime = Time.time + (1f / fireRate);
+    }
+
+    private bool CanShoot()
+    {
+        return Time.time >= fireTime;
+    }
+
+    private void RotateToMouse()
+    {
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        difference.Normalize();
+
+        float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        weaponPoint.rotation = Quaternion.Euler(0f, 0f, rotZ);
+    }
+}
